6450ce15f1eec1ca6417603fa02140533f46d277
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2016-01-27 19:00:06 -0200

    Modularize role testing.

--- test/domain/investments/roles/account_repository.rb
+++ test/domain/investments/roles/account_repository.rb
@@ -1,17 +1,19 @@
-module AccountRepository
-  def test_it_responds_to_create_account
-    assert_respond_to repository, :create_account
-  end
+module Investments
+  module AccountRepository
+    def test_it_responds_to_create_account
+      assert_respond_to repository, :create_account
+    end
 
-  def test_it_responds_to_update_amount
-    assert_respond_to repository, :update_amount
-  end
+    def test_it_responds_to_update_amount
+      assert_respond_to repository, :update_amount
+    end
 
-  def test_it_responds_to_create_asset
-    assert_respond_to repository, :create_asset
-  end
+    def test_it_responds_to_create_asset
+      assert_respond_to repository, :create_asset
+    end
 
-  def test_it_responds_to_destroy_asset
-    assert_respond_to repository, :destroy_asset
+    def test_it_responds_to_destroy_asset
+      assert_respond_to repository, :destroy_asset
+    end
   end
 end
