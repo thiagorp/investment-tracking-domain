3f50907e10a10059a1edabee6e97c7beeee33af2
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2016-01-28 21:30:15 -0200

    Test InvestorRepository#create_investor's arity.

--- lib/investment_tracking_domain/test_helpers/minitest/investments/roles/investor_repository_role_test.rb
+++ lib/investment_tracking_domain/test_helpers/minitest/investments/roles/investor_repository_role_test.rb
@@ -2,6 +2,7 @@ module Investments
   module InvestorRepositoryRoleTest
     def test_it_responds_to_create_investor
       assert_respond_to repository, :create_investor
+      assert_equal repository.method(:create_investor).arity, 1
     end
   end
 end
