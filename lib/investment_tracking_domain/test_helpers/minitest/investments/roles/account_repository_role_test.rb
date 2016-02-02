38a3a85a6b8ba03170adea0dd3c7d38370497ce4
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2016-02-02 17:07:01 -0200

    Update AccountRepositoryRole and AccountRepositoryFake.

--- lib/investment_tracking_domain/test_helpers/minitest/investments/roles/account_repository_role_test.rb
+++ lib/investment_tracking_domain/test_helpers/minitest/investments/roles/account_repository_role_test.rb
@@ -5,19 +5,9 @@ module Investments
       assert_equal repository.method(:create_account).arity, 1
     end
 
-    def test_it_responds_to_update_amount
-      assert_respond_to repository, :update_amount
-      assert_equal repository.method(:update_amount).arity, 1
-    end
-
-    def test_it_responds_to_create_asset
-      assert_respond_to repository, :create_asset
-      assert_equal repository.method(:create_asset).arity, 2
-    end
-
-    def test_it_responds_to_destroy_asset
-      assert_respond_to repository, :destroy_asset
-      assert_equal repository.method(:destroy_asset).arity, 1
+    def test_it_responds_to_update_account
+      assert_respond_to repository, :update_account
+      assert_equal repository.method(:update_account).arity, 1
     end
   end
 end
